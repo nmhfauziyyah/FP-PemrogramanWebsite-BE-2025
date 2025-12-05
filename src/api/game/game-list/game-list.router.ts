c8a30f17c6d627d6412fe17712b3cc7e7aca74f5
nmhfauziyyah <nmhfauziyyah@gmail.com>
2025-12-05 15:16:01 +0700

    add template anagram

--- src/api/game/game-list/game-list.router.ts
+++ src/api/game/game-list/game-list.router.ts
@@ -2,12 +2,14 @@
 /* eslint-disable import/no-default-export */
 import { Router } from 'express';
 
+import { AnagramController } from './anagram/anagram.controller';
 import { PairOrNoPairController } from './pair-or-no-pair/pair-or-no-pair.controller';
 import { QuizController } from './quiz/quiz.controller';
 
 const GameListRouter = Router();
 
 GameListRouter.use('/quiz', QuizController);
+GameListRouter.use('/anagram', AnagramController);
 GameListRouter.use('/pair-or-no-pair', PairOrNoPairController);
 
 export default GameListRouter;
